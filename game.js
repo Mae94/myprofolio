35b97b2413a7688cf793f433a8057002627f3977
Mae Briones <cmbriones94@gmail.com>
2019-06-26 13:22:59 +0900

    games

--- game.js
+++ game.js
@@ -1,18 +1,25 @@
+window.focus();
 enchant();
 
 window.onload = function(){
     
-    var core = new Core(200, 200);
+    var core = new Core(1110, 400);
 
     core.fps = 15;
 
-    core.preload('./images/chara1.png');
+    core.preload('./images/chara2.png', './images/monster/monster1.gif');
     core.onload = function(){
         var bear = new Sprite(32,32);
-        bear.image = core.assets['./images/chara1.png'];
+        bear.image = core.assets['./images/chara2.png'];
         
         bear.x = 100;
         bear.y = 0;
+
+        var enemy = new Sprite(50,50);
+        enemy.image = core.assets['./images/monster/monster1.gif'];
+        
+        enemy.x = 400;
+        enemy.y = 0;
 
         bear.addEventListener('enterframe',function(){
             // this.y += 10;
@@ -44,9 +51,10 @@ window.onload = function(){
         });
 
         core.rootScene.addChild(bear);
+        core.rootScene.addChild(enemy);
 
         var label = new Label();
-        label.x = 200;
+        label.x = 1000;
         label.y = 5;
         label.color ='red';
         label.font ='19px "Arial"';
