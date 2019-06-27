80e45264ce317f0b9bdbd58b2ee0195fb5dd821a
Mae Briones <cmbriones94@gmail.com>
2019-06-27 13:49:55 +0900

    update

--- game.js
+++ game.js
@@ -17,22 +17,68 @@ window.onload = function(){
         bear.x = 100;
         bear.y = 0;
 
-        for(var i = 0; i<10; i++){
+        var label = new Label();
+        label.x = 1000;
+        label.y = 5;
+        label.color ='red';
+        label.font ='19px "Arial"';
+
+        for(var i = 0; i<80; i++){
             var enemy = new Sprite(50,50);
         
             enemy.image = core.assets['./images/monster/monster1.gif'];
             
-            enemy.x = 400  + (i * 50);
-            enemy.y = (i * 50);
+            enemy.x = 800  + (i * 10);
+            enemy.y = (i * 10);
             enemy.frame = 2;
             core.rootScene.addChild(enemy);
+
+            enemy.addEventListener('enterframe',function(){
+
+                var min = 1 ;
+                var max = 3 ;
+                
+                var val = Math.floor( Math.random() * (max + 1 - min) ) + min ;
+
+                switch (val) {
+                    case 1:
+                        this.x -= 5;
+                        this.y += 2;
+                        break;
+
+                    case 2:
+                        this.x += 2;
+                        this.y -= 5;
+                        break;
+
+                    case 3:
+                        this.x -= 2;
+                        this.y += 5;
+                        break;
+                }
+
+                if(this.within(bear,10)){
+                    // label.text = 'HIT!'
+                    var gameover = new Label();
+                    gameover.x = 45;
+                    gameover.y = 120;
+                    gameover.color = "red";
+                    gameover.font = '40px "Arial"';
+                    gameover.text = 'GAME OVER';
+                    // var gameOverScene = new Scene();
+                    // gameOverScene.backgroundColor ='white';
+                    // core.pushScene(gameOverScene);
+                    core.rootScene.addChild(gameover);
+                    core.stop();
+                }
+
+            })
+
+            
         }
 
-        
-        
 
-       var gameOverScene = new Scene();
-       gameOverScene.backgroundColor ='white'; 
+        
 
  bear.addEventListener('enterframe',function(){
             // this.y += 10;
@@ -56,7 +102,7 @@ window.onload = function(){
             // label.text = 'HIT!'
             core.pushScene(gameOverScene);
             core.stop();
-            label.text = 'GAMEOVER'
+
             }
 
             bear.on('touchstart',function(){
@@ -68,18 +114,11 @@ window.onload = function(){
                 bear.y = e.y;
             })
         });
-        
-        
-       
-
+            
         core.rootScene.addChild(bear);
         
 
-        var label = new Label();
-        label.x = 1000;
-        label.y = 5;
-        label.color ='red';
-        label.font ='19px "Arial"';
+       
        
         label.on('enterframe',function(){
             label.text = core.frame;
